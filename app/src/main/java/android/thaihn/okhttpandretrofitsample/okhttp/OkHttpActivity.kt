6424879edad2d98af85ebb3b3fe3e3b345d13cc4
oHoangNgocThai <hoang.ngoc.thai@framgia.com>
2019-03-28 17:15:48 +0700

    Search repository with OkHttp part 1

--- app/src/main/java/android/thaihn/okhttpandretrofitsample/okhttp/OkHttpActivity.kt
+++ app/src/main/java/android/thaihn/okhttpandretrofitsample/okhttp/OkHttpActivity.kt
@@ -1,15 +1,31 @@
 package android.thaihn.okhttpandretrofitsample.okhttp
 
 import android.databinding.DataBindingUtil
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.thaihn.okhttpandretrofitsample.R
 import android.thaihn.okhttpandretrofitsample.databinding.ActivityOkHttpBinding
+import android.thaihn.okhttpandretrofitsample.entity.SearchResponse
+import android.thaihn.okhttpandretrofitsample.ui.RepositoryAdapter
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.gson.Gson
+import okhttp3.*
+import java.io.IOException
+import java.net.URL
 
 class OkHttpActivity : AppCompatActivity() {
 
+    companion object {
+        private val TAG = OkHttpActivity::class.java.simpleName
+    }
+
     private lateinit var mBinding: ActivityOkHttpBinding
+
+    private val mRepositoryAdapter = RepositoryAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +33,22 @@ class OkHttpActivity : AppCompatActivity() {
 
         supportActionBar?.title = "OkHttp Sample"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mBinding.rvRepo.apply {
+            adapter = mRepositoryAdapter
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        }
+
+        mBinding.apply {
+            btnSearch.setOnClickListener {
+                val name = edtInput.text.toString().trim()
+                searchRepository(name)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item?.itemId) {
+        when (item?.itemId) {
             android.R.id.home -> {
                 onBackPressed()
             }
@@ -32,4 +60,67 @@ class OkHttpActivity : AppCompatActivity() {
         onBackPressed()
         return true
     }
+
+    private fun searchRepository(name: String) {
+        if (name.isEmpty()) {
+            Toast.makeText(applicationContext, "Enter repository name", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        SearchAsyncWithOkHttp().execute(name)
+    }
+
+    inner class SearchAsyncWithOkHttp : AsyncTask<String, Int, SearchResponse>() {
+
+        override fun doInBackground(vararg params: String?): SearchResponse? {
+            var responseSearch: SearchResponse? = null
+            val key = params[0]
+
+            if (key != null) {
+                val request = createRequest(key)
+                OkHttpClient().newCall(request).enqueue(object : Callback {
+
+                    override fun onResponse(call: Call, response: Response) {
+                        val strResponse = response.body()?.string()?.trim()
+                        if (strResponse != null) {
+                            responseSearch = Gson().fromJson(strResponse, SearchResponse::class.java)
+                        }
+                    }
+
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                        Log.d(TAG, "onFailure: ${e.message}")
+                    }
+                })
+            }
+            return responseSearch
+        }
+
+        override fun onPostExecute(result: SearchResponse?) {
+            if (result != null) {
+                result.items?.let{
+                    mRepositoryAdapter.updateAllData(it)
+                }
+            }
+        }
+
+        private fun createRequest(key: String): Request {
+            val url = URL(createUrl("https://api.github.com/search/repositories", key))
+
+            return Request.Builder()
+                .header("Content-Type", "application/json")
+                .url(url)
+                .build()
+        }
+
+        private fun createUrl(root: String, key: String): String {
+            return HttpUrl.parse(root)?.newBuilder()?.apply {
+                addQueryParameter("q", key)
+                addQueryParameter("sort", "")
+                addQueryParameter("order", "desc")
+            }?.build().toString()
+        }
+    }
+
+
 }
